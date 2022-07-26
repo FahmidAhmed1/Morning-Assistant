8be00af0dccb3770d0514fb21423249e90be8763
Sagar Saha <59755509+SagarSaha01@users.noreply.github.com>
2022-07-26 19:31:26 -0400

    fix style

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
-
+ 
 const app = express();
 
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
